ce271e96835acd2c86c73e2dbbe53b9987b21509
Marco Di Blasi <marco.diblasi97@gmail.com>
2023-08-11 18:33:17 +0200

    Working on PC not on mobile

--- Frontend/StoreMDB/src/app/barcode-scanner/barcode-scanner.component.ts
+++ Frontend/StoreMDB/src/app/barcode-scanner/barcode-scanner.component.ts
@@ -1,43 +1,132 @@
-import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild, ElementRef, ChangeDetectorRef } from '@angular/core';
 import Quagga from '@ericblade/quagga2';
+import { getMainBarcodeScanningCamera } from '../camera-access';
 
 @Component({
   selector: 'app-barcode-scanner',
   templateUrl: './barcode-scanner.component.html',
   styleUrls: ['./barcode-scanner.component.css']
 })
-export class BarcodeScannerComponent {
+export class BarcodeScannerComponent implements AfterViewInit{
   constructor(
-    private scannerContainer: ElementRef
+    private scannerContainer: ElementRef,
+    private changeDetectorRef: ChangeDetectorRef
   ) { }
 
-  
-  ngOnInit() {
-    Quagga.init({
+  errorMessage: string | undefined;
+  started: boolean | undefined;
+
+  ngAfterViewInit(): void {
+    if (!navigator.mediaDevices || !(typeof navigator.mediaDevices.getUserMedia === 'function')) {
+      this.errorMessage = 'getUserMedia is not supported';
+      return;
+    }
+
+    this.initializeScanner();
+  }
+
+  private initializeScanner(): Promise<void> {
+    if (!navigator.mediaDevices || !(typeof navigator.mediaDevices.getUserMedia === 'function')) {
+      this.errorMessage = 'getUserMedia is not supported. Please use Chrome on Android or Safari on iOS';
+      this.started = false;
+      return Promise.reject(this.errorMessage);
+    }
+
+    // enumerate devices and do some heuristics to find a suitable first camera
+    return Quagga.CameraAccess.enumerateVideoDevices()
+      .then(mediaDeviceInfos => {
+        const mainCamera = getMainBarcodeScanningCamera(mediaDeviceInfos);
+        if (mainCamera) {
+          console.log(`Using ${mainCamera.label} (${mainCamera.deviceId}) as initial camera`);
+          return this.initializeScannerWithDevice(mainCamera.deviceId);
+        } else {
+          console.error(`Unable to determine suitable camera, will fall back to default handling`);
+          return this.initializeScannerWithDevice(undefined);
+        }
+      })
+      .catch(error => {
+        this.errorMessage = `Failed to enumerate devices: ${error}`;
+        this.started = false;
+      });
+  }
+
+  private initializeScannerWithDevice(preferredDeviceId: string | undefined): Promise<void> {
+    console.log(`Initializing Quagga scanner...`);
+
+    const constraints: MediaTrackConstraints = {};
+    if (preferredDeviceId) {
+      // if we have a specific device, we select that
+      constraints.deviceId = preferredDeviceId;
+    } else {
+      // otherwise we tell the browser we want a camera facing backwards (note that browser does not always care about this)
+      constraints.facingMode = 'environment';
+    }
+
+    return Quagga.init({
       inputStream: {
-        name: "Live",
-        type: "LiveStream",
-        // target: document.querySelector('#scannerContainer')    // Or '#yourElement' (optional)
-        target: this.scannerContainer.nativeElement
+        type: 'LiveStream',
+        constraints,
+        area: { // defines rectangle of the detection/localization area
+          top: '25%',    // top offset
+          right: '10%',  // right offset
+          left: '10%',   // left offset
+          bottom: '25%'  // bottom offset
+        },
+        target: document.querySelector('#scanner-container') ?? undefined
       },
       decoder: {
-        readers: ['ean_reader', 'code_128_reader']
-      }
-    },  (err) => {
-      if (err) {
-        console.log(err);
-        return
-      }
-      console.log("Initialization finished. Ready to start");
-      Quagga.start();
-      Quagga.onDetected((res) => {
-        console.log(res);
-        if (res.codeResult.code) {
-          this.onBarcodeScanned(res.codeResult.code);
+        readers: ['ean_reader'],
+        multiple: false
+      },
+      // See: https://github.com/ericblade/quagga2/blob/master/README.md#locate
+      locate: false
+    },
+      (err) => {
+        if (err) {
+          console.error(`Quagga initialization failed: ${err}`);
+          this.errorMessage = `Initialization error: ${err}`;
+          this.started = false;
+        } else {
+          console.log(`Quagga initialization succeeded`);
+          Quagga.start();
+          this.started = true;
+          this.changeDetectorRef.detectChanges();
+          Quagga.onDetected((res) => {
+            if (res.codeResult.code) {
+              this.onBarcodeScanned(res.codeResult.code);
+            }
+          });
         }
       });
-    });
   }
+
+
+  // ngOnInit() {
+  //   Quagga.init({
+  //     inputStream: {
+  //       name: "Live",
+  //       type: "LiveStream",
+  //       // target: document.querySelector('#scannerContainer')    // Or '#yourElement' (optional)
+  //       target: this.scannerContainer.nativeElement
+  //     },
+  //     decoder: {
+  //       readers: ['ean_reader', 'code_128_reader']
+  //     }
+  //   }, (err) => {
+  //     if (err) {
+  //       console.log(err);
+  //       return
+  //     }
+  //     console.log("Initialization finished. Ready to start");
+  //     Quagga.start();
+  //     Quagga.onDetected((res) => {
+  //       console.log(res);
+  //       if (res.codeResult.code) {
+  //         this.onBarcodeScanned(res.codeResult.code);
+  //       }
+  //     });
+  //   });
+  // }
 
   onBarcodeScanned(result: string) {
     console.log(result);
