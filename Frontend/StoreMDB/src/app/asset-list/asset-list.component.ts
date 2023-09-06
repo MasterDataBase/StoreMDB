d64fa7b61ddfaf395d2e49ead39beb00e3c28fe7
Marco Di Blasi <marco.diblasi97@gmail.com>
2023-09-06 18:37:49 +0200

    Working PDFMake

--- Frontend/StoreMDB/src/app/asset-list/asset-list.component.ts
+++ Frontend/StoreMDB/src/app/asset-list/asset-list.component.ts
@@ -3,6 +3,10 @@ import { Router } from '@angular/router';
 import { AssetInList, AssetsStore } from '../AssetsStore';
 import { HeroService } from '../hero.service';
 
+var pdfMake = require('pdfmake/build/pdfmake.js');
+var pdfFonts = require('pdfmake/build/vfs_fonts.js');
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+
 @Component({
   selector: 'app-asset-list',
   templateUrl: './asset-list.component.html',
@@ -15,13 +19,13 @@ export class AssetListComponent implements OnInit {
     private heroService: HeroService
   ) { }
 
-    assetsList: AssetInList[] = [];
+  assetsList: AssetInList[] = [];
 
   ngOnInit(): void {
     let assetToShow = this.heroService.GetListAsset();
     for (let i = 0; i < assetToShow.length; i++) {
       const element = assetToShow[i];
-      this.assetsList.push({asset:element, index: i+1, kit:"", note:""});
+      this.assetsList.push({ asset: element, index: i + 1, kit: "", note: "" });
     }
   }
 
@@ -29,5 +33,31 @@ export class AssetListComponent implements OnInit {
     this.router.navigate(['/barcode-scanner']);
   }
 
+  PrintPDF() {
+    let testBody = {
+      body: [
+        ['Column 1', 'Column 2', 'Column 3'],
+        ['One value goes here', 'Another one here', 'OK?']
+      ]
+    };
 
+    let bodyF = [{}];
+    //bodyF.push("Index", "SN","Name","Category","Status");
+
+    this.assetsList.forEach(elm =>
+      bodyF.push([elm.index, elm.asset.SN, elm.asset.name, elm.asset.category, elm.asset.status])
+    )
+
+    var dd = {
+      content: [
+        {
+          table:
+            testBody
+        }
+      ]
+    };
+
+    const pdfDocGenerator = pdfMake.createPdf(dd);
+    pdfDocGenerator.open();
+  }
 }
